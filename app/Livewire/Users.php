a2768b30130c1b05c411d9d3fd3a12ea28f0b440
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2025-12-18 12:00:00 +0700

    actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -8,6 +8,17 @@ use Livewire\Component;
 class Users extends Component
 {
     // public $title = "User Page - Livewire";
+
+    public function createUser()
+    {
+        // dd("button clicked!");
+        User::create([
+            "name" => "User " . rand(1, 100),
+            "email" => "user" . rand(1, 100) . "@example.com",
+            "password" => bcrypt("password")
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.users', [
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,5 +1,18 @@
-<div>
+<div class="w-1/2 m-auto my-10">
     {{-- Be like water. --}}
     <h1 class="text-3xl font-semibold text-blue-700">{{ $title }}</h1>
     <p>User Count: {{ count($users) }}</p>
+    <button type="button" wire:click="createUser"
+        class="my-2 px-5 py-2 cursor-pointer font-medium bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-sm">
+        Create User
+    </button>
+
+    <hr class="border-1 my-5">
+
+    <h2 class="text-2xl font-semibold mb-2">Users List</h2>
+    <ul class="list-disc">
+        @foreach ($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach
+    </ul>
 </div>
