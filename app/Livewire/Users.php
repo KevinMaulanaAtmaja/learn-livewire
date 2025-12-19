c428868dd34b51edf333d3384476bce75f1205a1
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2025-12-19 10:00:00 +0700

    form validations

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,24 +4,50 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Users extends Component
 {
     // public $title = "User Page - Livewire";
 
+    #[Validate('required|min:3')]
     public $name = '';
+
+    #[Validate('required|email:dns|unique:users')]
     public $email = '';
+
+    #[Validate('required|min:3')]
     public $password = '';
 
     public function createNewUser()
     {
+        // $validated = $this->validate([
+        //     'name' => 'required|min:3',
+        //     'email' => 'required|email:dns|unique:users',
+        //     'password' => 'required|min:3'
+        // ]);
+
         // dd("button clicked!");
+
+        // panggil validasi sblum kirim data
+        $this->validate();
+
         User::create([
             "name" => $this->name,
             "email" => $this->email,
             "password" => Hash::make($this->password)
         ]);
+
+        // alt
+        // User::create([
+        //     "name" => $validated['name'],
+        //     "email" => $validated['email'],
+        //     "password" => Hash::make($validated['password'])
+        // ]);
+
+        // alternatif
+        // User::create($validated);
 
         // $this->reset();
         $this->reset([
@@ -29,6 +55,8 @@ class Users extends Component
             "email",
             "password"
         ]);
+
+        session()->flash('success', 'User created successfully.');
     }
 
     public function render()
